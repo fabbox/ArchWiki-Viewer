18c9ae0e9eb921b8cff5665d538c3addafe1b739
fabbox <fbox.dev@gmail.com>
2014-10-21 23:57:38 +0200

    fix license header

--- www/app.js
+++ www/app.js
@@ -1,3 +1,24 @@
+/*
+Copyright (C) 2014 fbox
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/* 
+    Created on : 21 oct. 2014, 23:55:52
+    Author     : fbox
+*/
+
 // Be strict, do not try to understand my mistake and make error !
 'use strict';
 window.addEventListener("load", function () {
